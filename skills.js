e5ba24882328429c1624ca65992b955f51fd5d06
flyinyanzi <61301506+flyinyanzi@users.noreply.github.com>
2025-10-15 19:33:45 +0900

    Update skills.js

--- skills.js
+++ skills.js
@@ -5,15 +5,16 @@ const skills = [
     id: "feishazoushi",
     name: "飞沙走石",
     description: "把对方刚下的一个子扔出去",
-    trigger: "onUse",
+    usedBy: [],  // 标记使用者（1 或 2）
     effect: function (gameState) {
       const move = gameState.opponentLastMove;
-      if (!move) return;
+      if (!move) {
+        gameState.showDialog("你还没给我机会施展啊！");
+        return;
+      }
       gameState.board[move.y][move.x] = 0;
-      clearCellOnCanvas(move.x, move.y);
-      showDialog("飞沙走石！对方的棋子飞出去了！");
+      gameState.clearCell(move.x, move.y);
+      gameState.showDialog("飞沙走石发动！对方的棋子飞出去了！");
     }
   }
-
-  // 更多技能将加入此数组
 ];
