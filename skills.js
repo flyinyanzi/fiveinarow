d56692efb83d9a863fa0b3fa71f92345431594b5
flyinyanzi <61301506+flyinyanzi@users.noreply.github.com>
2025-10-15 20:24:48 +0900

    Update skills.js
    添加静如止水

--- skills.js
+++ skills.js
@@ -5,16 +5,22 @@ const skills = [
     id: "feishazoushi",
     name: "飞沙走石",
     description: "把对方刚下的一个子扔出去",
-    usedBy: [],  // 标记使用者（1 或 2）
+    usedBy: [],
     effect: function (gameState) {
       const move = gameState.opponentLastMove;
-      if (!move) {
-        gameState.showDialog("你还没给我机会施展啊！");
-        return;
-      }
       gameState.board[move.y][move.x] = 0;
       gameState.clearCell(move.x, move.y);
       gameState.showDialog("飞沙走石发动！对方的棋子飞出去了！");
+    }
+  },
+  {
+    id: "jingruzhishui",
+    name: "静如止水",
+    description: "使对方这轮无法下棋",
+    usedBy: [],
+    effect: function (gameState) {
+      gameState.skipNextTurn = true;
+      gameState.showDialog("静如止水发动！对方顿时凝固在原地！");
     }
   }
 ];
