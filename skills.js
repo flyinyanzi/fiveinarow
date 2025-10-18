400bfd5cf177b5387cfe4f2b0ba0dc73709f9a9f
flyinyanzi <61301506+flyinyanzi@users.noreply.github.com>
2025-10-18 22:46:35 +0900

    Refactor skill dialog display for current player
    添加梅开二度，擒拿，调虎离山

--- skills.js
+++ skills.js
@@ -10,7 +10,7 @@ const skills = [
       const move = gameState.opponentLastMove;
       gameState.board[move.y][move.x] = 0;
       gameState.clearCell(move.x, move.y);
-      gameState.showDialog("飞沙走石发动！对方的棋子飞出去了！");
+      gameState.showDialogForPlayer(gameState.currentPlayer, "飞沙走石发动！对方的棋子飞出去了！");
     }
   },
   {
@@ -20,7 +20,58 @@ const skills = [
     usedBy: [],
     effect: function (gameState) {
       gameState.skipNextTurn = true;
-      gameState.showDialog("静如止水发动！对方顿时凝固在原地！");
+      gameState.showDialogForPlayer(gameState.currentPlayer, "静如止水发动！对方顿时凝固在原地！");
+    }
+  },
+  {
+    id: "meikaierdhu",
+    name: "梅开二度",
+    description: "飞沙走石之后可再次使用一次飞沙走石",
+    usedBy: [],
+    visible: false,
+    dependsOn: "feishazoushi",
+    effect: function (gameState) {
+      const move = gameState.opponentLastMove;
+      gameState.board[move.y][move.x] = 0;
+      gameState.clearCell(move.x, move.y);
+      gameState.showDialogForPlayer(gameState.currentPlayer, "梅开二度！再飞你一次！");
+    }
+  },
+  {
+    id: "qin_na",
+    name: "擒拿",
+    description: "对方准备使用梅开二度时短时间内可反制，取消其技能",
+    usedBy: [],
+    visible: false,
+    triggeredBy: "meikaierdhu",
+    timeout: 3000,
+    effect: function (gameState) {
+      gameState.cancelOpponentSkill = true;
+      gameState.showDialogForPlayer(gameState.currentPlayer, "擒拿擒拿，擒擒又拿拿！");
+    }
+  },
+  {
+    id: "tiaohulishan",
+    name: "调虎离山",
+    description: "对方使用擒拿后短时间内可发动，偷取其两个棋子",
+    usedBy: [],
+    visible: false,
+    triggeredBy: "qin_na",
+    timeout: 3000,
+    effect: function (gameState) {
+      let removed = 0;
+      for (let y = 0; y < 15; y++) {
+        for (let x = 0; x < 15; x++) {
+          if (gameState.board[y][x] === 3 - gameState.currentPlayer) {
+            gameState.board[y][x] = 0;
+            gameState.clearCell(x, y);
+            removed++;
+            if (removed >= 2) break;
+          }
+        }
+        if (removed >= 2) break;
+      }
+      gameState.showDialogForPlayer(gameState.currentPlayer, "调虎离山成功，顺走对方两子！");
     }
   }
 ];
